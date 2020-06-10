0f65e2941b520c459eaa437521ae725529c7e1c0
Seda Erdogan <sedaerdogan@hotmail.de>
2020-06-10 20:35:46 +0200

    Steuerung umgeaendert, Springen klappt noch nicht aber die Bewegung ist nun leichter

--- PMM/Assets/Scripts/Movement.cs
+++ PMM/Assets/Scripts/Movement.cs
@@ -2,6 +2,8 @@
 using UnityEngine;
 
 public class Movement : MonoBehaviour{
+    public float moveSpeed;
+    public float jumpForce = 20;
 
     // This is a reference to the Rigidbody component called "cat"
     public Rigidbody cat;
@@ -11,29 +13,57 @@ public class Movement : MonoBehaviour{
 
     // We marked this as "Fixed"Update because we
     // are using it to deal with physics.
-    void FixedUpdate(){
-
-        if (Input.GetKey("w")){
-
-            // Add a force to the front
-            // Time.deltaTime to handle framerates
-            cat.AddForce(0, 0, -force * Time.deltaTime);
-        }
-
-        if (Input.GetKey("s")){
-
-            // Add a force to the back
-            cat.AddForce(0, 0, force * Time.deltaTime);
-        }
-
-        if (Input.GetKey("d")){ 
-            // Add a force to the right
-            cat.AddForce(-force * Time.deltaTime, 0, 0);
-        }
-
-        if (Input.GetKey("a")){
-            // Add a force to the left
-            cat.AddForce(force * Time.deltaTime, 0, 0);
-        }
+    void Start()
+    {
+        cat = GetComponent<Rigidbody>();
     }
+      
+    void Update()
+    {
+        cat.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, cat.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
+
+
+        // Hier sind 2 Spring Methoden die ich Probiert habe. Leider habe ich nicht verstanden wieso sie nicht Funktionieren. Werde versuchen sie zu verbessern sobald ich Zeit habe :) Vielleicht siehst du ja meinen Fehler.
+        // Der Code klappt auch wenn es einkommentiert ist, aber die katze springt einfach nicht :C
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    //Methode 1
+        //    Debug.Log("Moving: springen gedrückt");
+        //    cat.velocity = new Vector3(cat.velocity.x, force, cat.velocity.z);
+        //    Debug.Log("Moving: springt " + cat.velocity.y.ToString());
+
+
+        //    //Methode 2
+        //    //cat.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        //}
+    }
+
+
+
+
+    //void FixedUpdate(){
+
+    //    if (Input.GetKey("w")){
+
+    //        // Add a force to the front
+    //        // Time.deltaTime to handle framerates
+    //        cat.AddForce(0, 0, -force * Time.deltaTime);
+    //    }
+
+    //        if (Input.GetKey("s")){
+
+    //            // Add a force to the back
+    //            cat.AddForce(0, 0, force* Time.deltaTime);
+    //}
+
+    //    if (Input.GetKey("d")){ 
+    //        // Add a force to the right
+    //        cat.AddForce(-force * Time.deltaTime, 0, 0);
+    //    }
+
+    //    if (Input.GetKey("a")){
+    //        // Add a force to the left
+    //        cat.AddForce(force * Time.deltaTime, 0, 0);
+    //    }
+    //}
 }
